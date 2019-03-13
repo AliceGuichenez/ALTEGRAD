919d8859429c562db5450bd22e6b025bcbc82a71
Wicey35 <ntoussai29@gmail.com>
2019-03-13 11:27:55 +0100

    biased walk

---
+++ code/preprocessing_biased_walk.py
@@ -0,0 +1,131 @@
+import os
+import re
+import random
+import numpy as np
+import networkx as nx
+from time import time
+
+# = = = = = = = = = = = = = = = 
+
+# 'atoi' and 'natural_keys' taken from: https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split('(\d+)', text)]
+
+def random_walk(graph,node,walk_length):
+    walk = [node]
+    for i in range(walk_length):
+        neighbors = graph.neighbors(walk[i])
+        walk.append(random.choice(list(neighbors)))
+    return walk
+
+# modif 3
+# change those values
+q = 1.2
+p = np.random.uniform(min(q,1)-0.3, max(q,1)+0.3)
+def compute_probabilities(graph):
+    probs = {}
+    for source_node in graph.nodes():
+        probs[source_node] = {}
+        for current_node in graph.neighbors(source_node):
+            probs_ = []
+            for destination in graph.neighbors(current_node):
+                if source_node==destination:
+                    prob_ = graph[current_node][destination].get('weight',1) * (1/p)
+                elif destination in graph.neighbors(source_node):
+                        prob_ = graph[current_node][destination].get('weight',1)
+                else:
+                    prob_ = graph[current_node][destination].get('weight',1) * (1/q)
+                probs_.append(prob_)
+            probs[source_node][current_node] = probs_/np.sum(probs_)
+    return probs
+
+# modif 3                    
+def biased_walk(graph, node, walk_length, probs):
+    walk = [node]
+    walk_options = list(graph[node])
+    walk.append(random.choice(walk_options))
+    for i in range(walk_length-2):
+        walk_options = list(graph[walk[-1]])
+        probabilities = probs[walk[-2]][walk[-1]]
+        walk.append(np.random.choice(walk_options, p=probabilities))
+    return walk
+
+# modif 3
+def generate_walks(graph,num_walks,walk_length,biased=False):
+    '''
+    samples num_walks walks of length walk_length+1 from each node of graph
+    '''
+    if not biased:
+        graph_nodes = graph.nodes()
+        n_nodes = len(graph_nodes)
+        walks = []
+        for i in range(num_walks):
+            nodes = np.random.permutation(graph_nodes)
+            for j in range(n_nodes):
+                walk = random_walk(graph, nodes[j], walk_length)
+                walks.append(walk)
+        return walks
+    else:
+        graph_nodes = graph.nodes()
+        n_nodes = len(graph_nodes)
+        walks = []
+        #print("compute prob..")
+        probs = compute_probabilities(graph)
+        #print("compute walks..")
+        for i in range(num_walks):
+            nodes = np.random.permutation(graph_nodes)
+            for j in range(n_nodes):
+                walk = biased_walk(graph, nodes[j], walk_length, probs)
+                walks.append(walk)
+        return walks
+
+# = = = = = = = = = = = = = = =
+
+pad_vec_idx = 1685894 # 0-based index of the last row of the embedding matrix (for zero-padding)
+
+# parameters
+num_walks = 5
+walk_length = 10
+max_doc_size = 70 # maximum number of 'sentences' (walks) in each pseudo-document
+
+path_root = '..'
+path_to_data = path_root + '/data/'
+
+# = = = = = = = = = = = = = = =
+
+def main():
+
+    start_time = time() 
+
+    edgelists = os.listdir(path_to_data + 'edge_lists/')
+    edgelists.sort(key=natural_keys) # important to maintain alignment with the targets!
+
+    docs = []
+    N = len(edgelists)
+    for idx,edgelist in enumerate(edgelists):
+        g = nx.read_edgelist(path_to_data + 'edge_lists/' + edgelist) # construct graph from edgelist
+        doc = generate_walks(g,num_walks,walk_length, biased=True) # create the pseudo-document representation of the graph
+        docs.append(doc)
+        if idx % 50 == 0:
+            print("Computing graph {}/{}...".format(idx+1, N).ljust(10), end = "\r")
+
+    print('documents generated')
+    
+    # truncation-padding at the document level, i.e., adding or removing entire 'sentences'
+    docs = [d+[[pad_vec_idx]*(walk_length+1)]*(max_doc_size-len(d)) if len(d)<max_doc_size else d[:max_doc_size] for d in docs] 
+
+    docs = np.array(docs).astype('int')
+    print('document array shape:',docs.shape) # (93719, 70, 11)
+
+    np.save(path_to_data + 'documents_biased.npy', docs, allow_pickle=False)
+
+    print('documents saved')
+    print('everything done in', round(time() - start_time,2)) # 471.33
+
+# = = = = = = = = = = = = = = =
+
+if __name__ == '__main__':
+    main()
